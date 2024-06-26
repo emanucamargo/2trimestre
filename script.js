70b0c254a17f18d1fb658f39f244507d252809f0
emanucamargo <134521892+emanucamargo@users.noreply.github.com>
2024-06-26 11:56:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 const caixaPrincipal = document.querySelector(‘.caixaprincipal’);
-const caixaPerguntas = document.querySelector(‘.caixaperguntas’);
-const caixaAlternativas = document.querySelector(‘.caixaalternativas’);
-const caixaResultado = document.querySelector(‘.caixaresultado’);
-const textoResultado = document.querySelector(‘.textoresultado’);
-const lista = [item1, item2]
